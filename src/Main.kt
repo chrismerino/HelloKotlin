98d6865c25e2c2e0f623eb90ac08f05717876119
Cristian Merino <cris.merino@live.com>
2020-01-06 06:10:35 -0600

    "First commit"

--- src/Main.kt
+++ src/Main.kt
@@ -4,4 +4,20 @@
 
         print("Hello Kotlin")
 
+        serie()
+    }
+
+    fun serie(){
+
+        var numeroUno = 0
+        var numeroDos = 1
+        var sumaTotal: Int? = null
+
+        for (i in 1 .. 18) {
+            sumaTotal = numeroUno + numeroDos
+            println("$sumaTotal")
+            numeroDos++
+            numeroUno++
+        }
+
     }
